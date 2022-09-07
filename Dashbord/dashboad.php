cdb4cfbe69d131044b33f18e6143879f11a8e6c6
Shako Kinyamba <shakokinyamba201@gmail.com>
2022-09-07 18:55:56 +0200

    dashbord

---
+++ Dashbord/dashboad.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Styles/style.css">
+    <link rel="stylesheet" href="../fontawesome/css/all.min.css">
+    <title>menu</title>
+</head>
+
+<body>
+
+    <header class="head">
+    <span class="logo">
+        <i class="fas fa-search"></i>
+    </span>
+        <nav id="nav">
+            <ul class="list1 text-red-400" >
+                <li><a href="#">Accueil</a></li>
+                <li><a href="">Apropos</a></li>
+                <li><a href="">About</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main id="main">
+           <div>
+                <div id="sidebar">
+                    <span>username profil</span>
+                    <a class="btn">G Eleve</a>
+                    <a class="btn">G Enseignants</a>
+                    <a class="btn">G Frais s</a>
+                    <a class="btn">G Prime</a>
+                    <a class="btn">G Paiement</a>
+                    <a class="btn">G Classes</a>
+                    <a class="btn">G Options</a>
+                </div>
+           </div>
+        <section id="principal">
+            <div class="container-card">
+                    <div class="card">
+                        card
+                    </div>
+                    <div class="card">
+                        card
+                    </div>
+                    <div class="card">
+                        card
+                    </div>
+                    <div class="card">
+                        card
+                    </div>
+                    <div class="card">
+                        card
+                    </div>
+            </div>
+            <div class="tableau">
+                <div>
+                    <form action="" >
+                        <input type="text">
+                        <input type="date" name="" id="">
+                        <select name="" id="">
+                            <option value="">Noms eleve</option>
+                        </select>
+                    </form>
+                    <table>
+                        <thead>
+                            <tr>
+                            <th>Noms</th>
+                            <th>Postnom</th>
+                            <th>Postnom</th>
+                            <th>Postnom</th>
+                            <th>Postnom</th>
+                            <th>Postnom</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <td>Noms</td>
+                            <td>Postnom</td>
+                            <td>Postnom</td>
+                            <td>Postnom</td>
+                            <td>Postnom</td>
+                            <td>Postnom</td>
+                            </tr>
+                            <tr>
+                            <td>Noms</td>
+                            <td>Postnom</td>
+                            <td>Postnom</td>
+                            <td>Postnom</td>
+                            <td>Postnom</td>
+                            <td>Postnom</td>
+                            </tr>
+                            <tr>
+                            <td>Noms</td>
+                            <td>Postnom</td>
+                            <td>Postnom</td>
+                            <td>Postnom</td>
+                            <td>Postnom</td>
+                            <td>Postnom</td>
+                            </tr>
+                            <tr>
+                            <td>Noms</td>
+                            <td>Postnom</td>
+                            <td>Postnom</td>
+                            <td>Postnom</td>
+                            <td>Postnom</td>
+                            <td>Postnom</td>
+                            </tr>
+                            <tr>
+                            <td>Noms</td>
+                            <td>Postnom</td>
+                            <td>Postnom</td>
+                            <td>Postnom</td>
+                            <td>Postnom</td>
+                            <td>Postnom</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+
+</html>
