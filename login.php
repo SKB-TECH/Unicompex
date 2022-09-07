5ca9e439b45dd401186ad678c3f3992e36f2d79c
Doddy MATABARO <matabarododdy@gmail.com>
2022-09-07 02:12:32 +0200

    refact: connexiondb to oop tips

---
+++ login.php
@@ -0,0 +1,32 @@
+<?php
+    error_reporting( E_ALL );
+	ini_set( 'display_errors', 1);
+    include('outils/connexionBdd.php');
+  
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./Styles/index.css">
+    <title>Nyalukemba: Login</title>
+</head>
+<body class="grid ">
+    <header>
+            <div>logo</div>
+            <nav>Menu</nav>
+    </header>
+    <main>
+        <aside>
+            side bar
+        </aside>
+        <section id="content">
+                content
+        </section>
+    </main>
+</body>
+</html>
--- outils/connexionBdd.php
+++ outils/connexionBdd.php
@@ -1,7 +1,26 @@
 <?php 
-        try{
-            $pdo =new PDO("mysql:host=localhost;dbname=nyalukemba_db","root","");
-        }catch(Exception $e){
-            die('Erreur de connexion :' .$e->getMessage());
-        }
+ class Dbconnect 
+ { 
+  private $_localhost = 'localhost'; 
+  private $_user = 'root'; 
+  private $_password = ''; 
+  private $_dbname = 'nyalukemba_db'; 
+   
+  protected $connection; 
+   
+   public function __construct() 
+   { 
+   
+   if(!isset($this-> connection)) 
+   { 
+               
+               $this->connection = new PDO("mysql:host=".$this->_localhost.";dbname=".$this->_dbname."", $this->_user , $this->_password); 
+   
+   } 
+   
+   return $this->connection; 
+   
+   } 
+
+ } 
 ?>
