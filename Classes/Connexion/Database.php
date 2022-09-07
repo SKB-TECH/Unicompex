aff46556d7e0351781657db1c5156c23e1e63a56
Doddy MATABARO <matabarododdy@gmail.com>
2022-09-07 04:01:10 +0200

    user

--- Classes/Connexion/Database.php
+++ Classes/Connexion/Database.php
@@ -4,22 +4,27 @@
  */
     class Database
     {
-        private $dns="mysql:localhost,dbname=nyalukemba_db";
-        private $user="root";
-        private $pass="";
-        private $conn="";
-
-        public function __construct()
-            { 
-                try {
-                    $this->conn=new PDO($this->dns,$this->user,$this->pass);
-                    echo("connexion succefuly");
-                } 
-                catch (PDOException $msg) {
-                    echo ($msg->getMessage());
-                }
-            }
+        private $_localhost = 'localhost'; 
+        private $_user = 'root'; 
+        private $_password = ''; 
+        private $_dbname = 'nyalukemba_db'; 
+         
+        protected $connection; 
+         
+         public function __construct() 
+         { 
+         
+         if(!isset($this-> connection)) 
+         { 
+                     
+                $this->connection = new PDO("mysql:host=".$this->_localhost.";dbname=".$this->_dbname."", $this->_user , $this->_password); 
+         
+         } 
+         
+         return $this->connection; 
+         
+         } 
     }
 
-    $bdd=new Database()
+    // $bdd=new Database()
 ?>
