bef0155d590e3958bf563117422af9c925595a4f
Doddy MATABARO <matabarododdy@gmail.com>
2022-09-17 08:07:40 +0200

    pdf files suit

--- pages/impressions/caisse.php
+++ pages/impressions/caisse.php
@@ -59,7 +59,6 @@ class myPDF extends FPDF {
          $this->Cell(30,7,$total." ".$devise,1,0,'C', true);
     }
     // fin table perception
-   
 
 
     // entete du tableau des paie agent
@@ -77,24 +76,23 @@ class myPDF extends FPDF {
    
     function rowsPaie($db){
         $i = 0;
-        $total = 0;
-        $res=$db->selectalldata2("SELECT *, sum(montant)  as salaire, sum(avance)  as avance,sum(mituelle)  as mituelle    FROM perception 
-            inner join frais on frais.id = idFrais 
-            group by idFrais 
-            order by libelle asc");
-        while($data = $res->fetch()){
-            $i++;
+        $res=$db->selectalldata2("SELECT *, sum(montant)  as salaire, sum(avance)  as avance,sum(mituelle)  as mituelle    FROM paie ");
+        $data = $res->fetch();
             $this->SetFont('times','',12);
             $this->SetFillColor(255, 255, 255);
-            $this->Cell(15,7,utf8_decode($i),1,0,'C', true);
-            $this->Cell(40,7,utf8_decode($data['libelle']),1,0,'L', true);
-            $this->Cell(30,7,$data['somme']." ".$data['devise'],1,0,'C', true);
-            $total += $data['somme'];
-            $devise = $data['devise'];
+            $this->Cell(15,7,utf8_decode(1),1,0,'C', true);
+            $this->Cell(40,7,utf8_decode("Salaire"),1,0,'C', true);
+            $this->Cell(30,7,$data['salaire'],1,0,'L', true);
+            
+            $this->Cell(15,7,utf8_decode(1),1,0,'C', true);
+            $this->Cell(40,7,utf8_decode("Mituelle"),1,0,'C', true);
+            $this->Cell(30,7,$data['mituelle'],1,0,'L', true);
+
+            $total = $data['salaire'] -  $data['mituelle'];
             $this->Ln();
-        }
-         $this->Cell(55,7,"TOTAL",1,0,'C', true);
-         $this->Cell(30,7,$total." ".$devise,1,0,'C', true);
+        
+         $this->Cell(55,7,"TOTAL(S-M)",1,0,'C', true);
+         $this->Cell(30,7,$total." $",1,0,'C', true);
     }
     // fin du tableau de paie agent
     
@@ -119,10 +117,14 @@ $pdf->Table($db);
 $pdf->Ln(7);
 $pdf->headerPerception();
 $pdf->Cell(50,10,'',0,0);
-$pdf->headerTable2();
+$pdf->headerPaie();
 $pdf->Ln(7);
 $pdf->rowsPerception($db);
-$pdf->Ln(5);
+// $pdf->Cell(50,10,'',0,0);
+// $pdf->rowsPaie($db);
+// $pdf->Ln(5);
+
+
 $pdf->Totalite($db);
 $pdf->Ln(5);
 $pdf->Cell(450,5,utf8_decode("Fait à Bukavu le ").date('d/m/Y'),0,0,'C');
